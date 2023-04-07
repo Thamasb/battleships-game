b6b7690f0162ae8e9a91ff665e7fed2b1af74a9e
Tamas Baranyai <thamasb@gmail.com>
2023-04-07 19:42:27 +0000

    Add grid size and number of ships, define the symbols

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Define the grid size
+GRID_SIZE = 10
+
+# Define the number of ships
+NUM_SHIPS = 5
+
+# Define the ship symbol
+SHIP_SYMBOL = "S"
+
+# Define the hit symbol
+HIT_SYMBOL = "X"
+
+# Define the miss symbol
+MISS_SYMBOL = "O"
+
