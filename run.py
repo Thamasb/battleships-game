5257233baad2308211213531fda00a86a5998233
Tamas Baranyai <thamasb@gmail.com>
2023-04-09 11:21:44 +0000

    Fix error message around the final message

--- run.py
+++ run.py
@@ -104,4 +104,5 @@ while num_turns < 10:
 
 if (not win):
     print("FAILED! You could not sink all the ships in given shots. Try again")
-    print("You sunk"+str(NUM_SHIPS-count_hit_ships(grid))+" ships out of "+str(NUM_SHIPS)+"ships")
+    print("You sunk"+str(NUM_SHIPS-count_hit_ships(grid)) + 
+          "ships out of" + str(NUM_SHIPS)+"ships")
