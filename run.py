cbaf7bcc22302da24fe178d093899acaf17408bf
Tamas Baranyai <thamasb@gmail.com>
2023-04-13 20:43:55 +0000

    Remove comments from python file

--- run.py
+++ run.py
@@ -1,21 +1,11 @@
 from random import randint
 
-# Define the grid size
 GRID_SIZE = 10
-
-# Define the number of ships
 NUM_SHIPS = 5
-
-# Define the ship symbol
 SHIP_SYMBOL = "S"
-
-# Define the hit symbol
 HIT_SYMBOL = "X"
-
-# Define the miss symbol
 MISS_SYMBOL = "O"
 
-# Create the grid
 grid = []
 for i in range(GRID_SIZE):
     row = []
@@ -23,7 +13,6 @@ for i in range(GRID_SIZE):
         row.append(" ")
     grid.append(row)
 
-# Place the ships on the grid
 for i in range(NUM_SHIPS):
     ship_placed = False
     while not ship_placed:
